87c7a8b3bff174851fca7e69989dafd0b98f8ed5
itskaffeine <hughmanizer@gmail.com>
2026-04-21 04:24:39 -0600

    Misc plotting tweaks & fixes

--- mission_err.py
+++ mission_err.py
@@ -6,6 +6,7 @@ import transformations as trans
 import orbit_engine as oe
 import spacecraft_engine as se
 import plotly.graph_objects as go
+import plotly.io as pio
 from PIL import Image
 
 lmo = oe.Orbit(c.LMO_RADIUS,c.LMO_RAAN,c.LMO_INCL,c.NS_INIT_TRUE_LAT,c.LMO_ORBIT_RATE,c.PLANET_MU)
@@ -17,8 +18,13 @@ sigma_sun_err_hist = x_err_task11[:,:3]
 omega_sun_err_hist = x_err_task11[:,3:]
 sigma_sun_err_hist_norm = np.linalg.norm(sigma_sun_err_hist, axis=1)
 omega_sun_err_hist_norm = np.linalg.norm(omega_sun_err_hist, axis=1)
+control_norm = np.linalg.norm(u_task11, axis=1)
+att_norm = np.linalg.norm(x_task11[:,3:], axis=1)
 
 t_arr = np.linspace(0, 6500, 100) #7102 #88620
+
+indices = [i for i in range(len(mode_task11) - 1) if mode_task11[i] != mode_task11[i+1]]
+print(indices)
 
 lmo_pos_list, lmo_vel_list = [], []
 gmo_pos_list, gmo_vel_list = [], []
@@ -45,7 +51,7 @@ lmo_omega_nadir = np.array(lmo_omega_nadir_list)
 lmo_omega_comms = np.array(lmo_omega_comms_list)
 
 def finalize_plot(ylabel):
-    plt.ylabel(ylabel, fontsize=12)
+    plt.ylabel(ylabel, fontsize=12, wrap=True)
     plt.xlabel(r'Time $[s]$', fontsize=12)
     plt.legend(loc='lower right')
     plt.grid(True, linestyle='--', alpha=0.7)
@@ -62,17 +68,17 @@ plt.subplot(2,2,2)
 plt.plot(t_task11, omega_sun_err_hist[:,0], label=r'$\omega_1$')
 plt.plot(t_task11, omega_sun_err_hist[:,1], label=r'$\omega_2$')
 plt.plot(t_task11, omega_sun_err_hist[:,2], label=r'$\omega_3$')
-finalize_plot(r'Angular Velocity Errors [$rad/s$]')
+finalize_plot(r'Inertial Angular Velocity Errors [$rad/s$]')
 
 plt.subplot(2,2,3)
-plt.plot(t_task11, sigma_sun_err_hist_norm, label=r'$|\sigma|$')
+plt.plot(t_task11, sigma_sun_err_hist_norm)
 threshold = sigma_sun_err_hist_norm[0] / np.exp(1)
 finalize_plot(r'Attitude Error Magnitude')
 
 plt.subplot(2,2,4)
-plt.plot(t_task11, omega_sun_err_hist_norm, label=r'$|\omega|$')
+plt.plot(t_task11, omega_sun_err_hist_norm)
 threshold = omega_sun_err_hist_norm[0] / np.exp(1)
-finalize_plot(r'Angular Velocity Error Magnitude [$rad/s$]')
+finalize_plot(r'Inertial Angular Velocity Error Magnitude [$rad/s$]')
 plt.show()
 
 # LMO Mission Pointing
@@ -84,7 +90,32 @@ finalize_plot(r'Attitude')
 
 plt.subplot(2,1,2)
 plt.plot(t_task11, mode_task11[:])
-finalize_plot(r'Mode')
+ticks = [-1, 0, 1]
+labels = ['GMO-pointing', 'Sun-pointing', 'Nadir-pointing']
+plt.yticks(ticks, labels)
+finalize_plot(r'Mission Mode')
+plt.show()
+
+# Control Plotting
+plt.subplot(2,2,1)
+plt.plot(t_task11, x_task11[:,3], label=r'$\omega_1$')
+plt.plot(t_task11, x_task11[:,4], label=r'$\omega_2$')
+plt.plot(t_task11, x_task11[:,5], label=r'$\omega_3$')
+finalize_plot(r'Inertial Angular Velocity [$rad/s$]')
+
+plt.subplot(2,2,2)
+plt.plot(t_task11, u_task11[:,0], label=r'$\tau_1$')
+plt.plot(t_task11, u_task11[:,1], label=r'$\tau_2$')
+plt.plot(t_task11, u_task11[:,2], label=r'$\tau_3$')
+finalize_plot(r'Control Torque [$Nm$]')
+
+plt.subplot(2,2,3)
+plt.plot(t_task11, att_norm, label=r'$\omega_1$')
+finalize_plot(r'Inertial Angular Velocity Magnitude [$rad/s$]')
+
+plt.subplot(2,2,4)
+plt.plot(t_task11, control_norm[:], label=r'$\tau$')
+finalize_plot(r'Control Torque Magnitude [$Nm$]')
 plt.show()
 
 # Mars Trajectory Plotting
@@ -120,29 +151,56 @@ fig.add_trace(go.Surface(
 # Add Trajectories
 fig.add_trace(go.Scatter3d(
     x=lmo_pos[:,0], y=lmo_pos[:,1], z=lmo_pos[:,2],
-    mode='lines',
+    mode='lines+text',
     line=dict(color='blue', width=5),
-    name='LMO'
+    name='LMO',
+    text=[None] * (len(lmo_pos)-1) + ['LMO'], 
+    textposition="top center",
+    textfont=dict(size=18, color='blue'),
+    showlegend=False
 ))
 
 fig.add_trace(go.Scatter3d(
     x=gmo_pos[:,0], y=gmo_pos[:,1], z=gmo_pos[:,2],
-    mode='lines',
+    mode='lines+text',
     line=dict(color='orange', width=5),
-    name='GMO'
+    name='GMO',
+    text=[None] * (len(gmo_pos)-1) + ['GMO'], 
+    textposition="top center",
+    textfont=dict(size=18, color='orange'),
+    showlegend=False
 ))
 
 # 5. Perfect Aspect Ratio & Dark Theme
 fig.update_layout(
-    template="plotly_dark",
     scene=dict(
-        aspectmode='data', # This prevents stretching automatically
-        xaxis_title='X (km)',
-        yaxis_title='Y (km)',
-        zaxis_title='Z (km)',
+        aspectmode='data',
+        xaxis=dict(
+            title=dict(text='<br>n1 [km]<br>', font=dict(size=22)),
+            tickfont=dict(size=16)
+        ),
+        yaxis=dict(
+            title=dict(text='<br>n2 [km]<br>', font=dict(size=22)),
+            tickfont=dict(size=16)
+        ),
+        zaxis=dict(
+            title=dict(text='<br>n3 [km]<br>', font=dict(size=22)),
+            tickfont=dict(size=16)
+        ),
         camera=dict(eye=dict(x=1.5, y=1.5, z=0.8))
     ),
-    margin=dict(l=0, r=0, b=0, t=0)
+    # Ensure legend is visible and styled
+    showlegend=True,
+    legend=dict(
+        font=dict(size=18),
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        bgcolor="rgba(255, 255, 255, 0.5)" # Semi-transparent background
+    ),
+    margin=dict(l=80, r=80, b=80, t=80)
 )
 
-fig.show()
+pio.renderers.default = 'browser'
+fig.show(include_mathjax='cdn')
--- sun_err.py
+++ sun_err.py
@@ -18,7 +18,7 @@ sigma_sun_err_hist_norm = np.linalg.norm(sigma_sun_err_hist, axis=1)
 omega_sun_err_hist_norm = np.linalg.norm(omega_sun_err_hist, axis=1)
 
 def finalize_plot(ylabel):
-    plt.ylabel(ylabel, fontsize=12)
+    plt.ylabel(ylabel, fontsize=12, wrap=True)
     plt.xlabel(r'Time $[s]$', fontsize=12)
     plt.legend(loc='lower right')
     plt.grid(True, linestyle='--', alpha=0.7)
@@ -37,17 +37,25 @@ plt.plot(t_task8, omega_sun_err_hist[:,1], label=r'$\omega_2$')
 plt.plot(t_task8, omega_sun_err_hist[:,2], label=r'$\omega_3$')
 finalize_plot(r'Angular Velocity Errors [$rad/s$]')
 
-plt.subplot(2,2,3)
-plt.plot(t_task8, sigma_sun_err_hist_norm, label=r'$|\sigma|$')
+ax = plt.subplot(2,2,3)
+plt.plot(t_task8, sigma_sun_err_hist_norm)
 threshold = sigma_sun_err_hist_norm[0] / np.exp(1)
 plt.axhline(threshold, color='r', linestyle='--', zorder=5) 
+ax.text(120, 1.02, '120', transform=ax.get_xaxis_transform(), 
+        ha='center')
 plt.axvline(120, color='r', linestyle='--', zorder=5)
+ax.text(1.01, threshold, r'$|\sigma|_0/e$', transform=ax.get_yaxis_transform(), 
+        va='center')
 finalize_plot(r'Attitude Error Magnitude')
 
-plt.subplot(2,2,4)
-plt.plot(t_task8, omega_sun_err_hist_norm, label=r'$|\omega|$')
+ax = plt.subplot(2,2,4)
+plt.plot(t_task8, omega_sun_err_hist_norm)
 threshold = omega_sun_err_hist_norm[0] / np.exp(1)
 plt.axhline(threshold, color='r', linestyle='--', zorder=5) 
+ax.text(120, 1.02, '120', transform=ax.get_xaxis_transform(), 
+        ha='center')
 plt.axvline(120, color='r', linestyle='--', zorder=5)
+ax.text(1.01, threshold, r'$|\omega|_0/e$', transform=ax.get_yaxis_transform(), 
+        va='center')
 finalize_plot(r'Angular Velocity Error Magnitude [$rad/s$]')
 plt.show()
